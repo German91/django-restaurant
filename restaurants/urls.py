81fb5a77cdc97616e2e302ec813f150949d8aa74
German <germanhernandez.dr@gmail.com>
2017-07-10 21:40:38 +0100

    Added restaurant view, update restaurants

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -5,4 +5,6 @@ from restaurants import views
 urlpatterns = [
   url(r'^$', views.restaurant_list, name='restaurant_list'),
   url(r'^add/$', views.restaurant_add, name='restaurant_add'),
+  url(r'^(?P<slug>[\w-]+)/$', views.restaurant_view, name='restaurant_view'),
+  url(r'(?P<pk>\d+)/archive/$', views.restaurant_archive, name='restaurant_archive'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -7,16 +7,34 @@ from .models import Restaurant
 
 @login_required
 def restaurant_list(request):
-  restaurants = Restaurant.objects.all()
-  return render(request, 'restaurants/restaurant_list.html', {'restaurants': restaurants})
+    restaurants = Restaurant.objects.filter(archived=False)
+    return render(request, 'restaurants/restaurant_list.html', {'restaurants': restaurants})
 
 @login_required
 def restaurant_add(request):
-  form = AddRestaurantForm()
-  if request.method == 'POST':
+    form = AddRestaurantForm()
+    if request.method == 'POST':
         form = AddRestaurantForm(request.POST)
         if form.is_valid():
-              form.save(commit=True)
-              messages.add_message(request, messages.SUCCESS, 'Restaurant successfully added')
-              return redirect('restaurant_list')
-  return render(request, 'restaurants/restaurant_add.html', {'form': form})
+            form.save(commit=True)
+            messages.add_message(request, messages.SUCCESS, 'Restaurant successfully added')
+            return redirect('restaurant_list')
+    return render(request, 'restaurants/restaurant_add.html', {'form': form})
+
+@login_required
+def restaurant_view(request, slug):
+    restaurant = get_object_or_404(Restaurant, slug=slug)
+    form = AddRestaurantForm(instance=restaurant)
+    if request.method == 'POST':
+        form = AddRestaurantForm(request.POST, instance=restaurant)
+        if form.is_valid():
+            form.save(commit=True)
+            messages.add_message(request, messages.SUCCESS, 'Restaurant successfully updated')
+    return render(request, 'restaurants/restaurant_view.html', {'restaurant': restaurant, 'form': form})
+
+@login_required
+def restaurant_archive(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    restaurant.archived = True
+    restaurant.save()
+    return redirect('restaurant_list')
