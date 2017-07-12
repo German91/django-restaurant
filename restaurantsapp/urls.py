a821257f4a604e60b01d079947e2c29487b9db83
German Hernandez <german@gearedapp.co.uk>
2017-07-12 15:39:24 +0100

    Added image to restaurant_list template, added url to media fiels

--- restaurantsapp/urls.py
+++ restaurantsapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,4 +29,4 @@ urlpatterns = [
     url(r'^sections/', include('sections.urls')),
     url(r'^categories/', include('categories.urls')),
     url(r'^items/', include('items.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
