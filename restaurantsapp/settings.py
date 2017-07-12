f3daef73b5492def40fc4ac001e7d32f5b7366b6
German Hernandez <german@gearedapp.co.uk>
2017-07-12 15:20:42 +0100

    Added media folder to gitignore, deleted aws settings and changed the content to the settings main file

--- restaurantsapp/settings.py
+++ restaurantsapp/settings.py
@@ -159,8 +159,12 @@ ALLOWED_HOSTS = ['localhost', '121.0.0.1', 'django-restaurants.herokuapp.com']
 
 DEBUG = True
 
-if os.environ.get('ENV_VAR') == 'prod':
-    from aws_settings import *
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/uploads/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 try:
     from .local_settings import *
